1beab3ba6c39d2a6a397fb533f9e76ca81c4e27c
Alex <alex134_16_zgz@hotmail.com>
2018-05-28 12:20:48 +0200

    Adding stuff

--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
