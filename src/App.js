c4103afa8a8b3f235862ad42414de3161ee0cbe5
Alex <alex134_16_zgz@hotmail.com>
2018-05-28 16:09:08 +0200

    trying some stuff

--- src/App.js
+++ src/App.js
@@ -6,37 +6,38 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
-        <div class="jumbotron text-center">
-          <h1>My First Bootstrap Page</h1>
-          <p>Resize this responsive page to see the effect!</p>
-        </div>
-        
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-4">
-              <h3>Column 1</h3>
-              <p>Lorem ipsum dolor..</p>
-              <p>Ut enim ad..</p>
-            </div>
-            <div class="col-sm-4">
-              <h3>Column 2</h3>
-              <p>Lorem ipsum dolor..</p>
-              <p>Ut enim ad..</p>
-            </div>
-            <div class="col-sm-4">
-              <h3>Column 3</h3>
-              <p>Lorem ipsum dolor..</p>
-              <p>Ut enim ad..</p>
+        <div name="body" class="w3-light-grey">
+          <div class="jumbotron text-center">
+            <h1>My First Bootstrap Page</h1>
+            <p>Resize this responsive page to see the effect!</p>
+          </div>
+          
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-4">
+                <h3>Column 1</h3>
+                <p>Lorem ipsum dolor..</p>
+                <p>Ut enim ad..</p>
+              </div>
+              <div class="col-sm-4">
+                <h3>Column 2</h3>
+                <p>Lorem ipsum dolor..</p>
+                <p>Ut enim ad..</p>
+              </div>
+              <div class="col-sm-4">
+                <h3>Column 3</h3>
+                <p>Lorem ipsum dolor..</p>
+                <p>Ut enim ad..</p>
+              </div>
             </div>
           </div>
+
+
+          <footer class="w3-container w3-pale-green w3-center w3-margin-top">
+            <p>Social media</p>
+            <p>Alejandro Gálvez</p>
+          </footer>
         </div>
-
-
-        <footer class="w3-container w3-teal w3-center w3-margin-top">
-          <p>Social media</p>
-          <p>Alejandro Gálvez</p>
-        </footer>
       </div>
     );
   }
